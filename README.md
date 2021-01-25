d5e8ed3275e26ef25ef5dc0f630d1d2f1e2d38c6
badal sarkar <badalsarkar86@gmail.com>
2021-01-25 08:42:11 -0500

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# My Blog
-
-This is my blog created with Gatsby blog starter template and customized by me.
-
-In this blog, I write about the things I am learning and working with.
-
-
---
+++ component---src-pages-404-js-1309b621fa9168260594.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(t,e,a){"use strict";a.r(e);var n=a("q1tI"),l=a.n(n),o=a("Bl7J"),i=a("vrFN");e.default=function(t){var e=t.data,a=t.location,n=e.site.siteMetadata.title;return l.a.createElement(o.default,{location:a,title:n},l.a.createElement(i.a,{title:"404: Not Found"}),l.a.createElement("h1",null,"404: Not Found"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-1309b621fa9168260594.js.map
